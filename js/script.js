bf2efd228e9d335e852e3066a34d5267cecade08
AbbyAGarcia <agarcia91@hawk.iit.edu>
2025-04-15 22:40:45 -0500

    edits to map function in script

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
-var map;
-function initMap(){
-  
-  map = new google.maps.Map(document.getElementbyId('map'),{
-    center: {lat: 23.5558, lng: 46.6396}
-    zoom: 8
-  }
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
 }
+
+initMap();
     
     
